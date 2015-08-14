b72c8e43b90cf65591a39c9ed961a3af5b3de547
Yasmany Cubela Medina <ycubela@teravisiontech.com>
2015-08-14 15:53:13 -0430

    adding some code and files

--- framework/src/AppBundle/Controller/DefaultController.php
+++ framework/src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Instance;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +16,32 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
         ));
+    }
+
+    private function getInstances(){
+        $doctrine = $this->getDoctrine();
+        return $doctrine->getRepository("AppBundle:Instance")->findAll();
+    }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/dashboard.html.twig', ["instances"=>$this->getInstances()]);
+    }
+
+    /**
+     * @Route("/display", name="display")
+     */
+    public function displayAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        $response = $this->render('default/display.html.twig', ["hostName"=>gethostname()]);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 }
