44e0f2b379246be2d52abb2d30f63e0f56b9c439
Yasmany Cubela Medina <ycubela@teravisiontech.com>
2015-08-17 11:44:13 -0430

    adding scale up and down

--- framework/src/AppBundle/Controller/DefaultController.php
+++ framework/src/AppBundle/Controller/DefaultController.php
@@ -3,23 +3,12 @@
 namespace AppBundle\Controller;
 
 use GuzzleHttp\Client as GuzzleHttpClient;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-        ));
-    }
-
     private function getInstances()
     {
         $client = new GuzzleHttpClient();
@@ -34,24 +23,12 @@ class DefaultController extends Controller
         return $response;
     }
 
-    private function renderInstances($instances)
-    {
-        $renders = [];
-        foreach ($instances as $instance) {
-            $client = new GuzzleHttpClient();
-            $response = $client->get(sprintf('http://%s/app_dev.php/display', $instance));
-            $renders[] = $response->getBody()->getContents();
-        }
-        return $renders;
-    }
-
     /**
-     * @Route("/dashboard", name="dashboard")
+     * @Route("/", name="homepage")
      */
-    public function dashboardAction(Request $request)
+    public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/dashboard.html.twig', ["renders" => $this->renderInstances($this->getInstances())]);
+        return $this->render('default/index.html.twig', ["instances" => $this->getInstances()]);
     }
 
     /**
@@ -59,6 +36,9 @@ class DefaultController extends Controller
      */
     public function displayAction(Request $request)
     {
-        return $this->render('default/display.html.twig', ["hostName" => gethostname()]);
+        // replace this example code with whatever you need
+        $response = $this->render('default/display.html.twig', ["hostName"=>gethostname()]);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 }
