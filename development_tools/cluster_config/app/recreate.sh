db4b19bb7a246185b67f6e079e9b0deb1472378a
Yasmany Cubela Medina <ycubela@teravisiontech.com>
2015-09-03 13:22:36 -0430

    adding mysql container adding volume to app rc so can use it as dvelopment env updating sf vendors

--- development_tools/cluster_config/app/recreate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-bash build-image.sh
-bash delete.sh
-bash create.sh
--- development_tools/cluster_config/app/scale-down.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl -s http://127.0.0.1:8080 scale rc --replicas=1 k8s-php-test
