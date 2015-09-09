34321ec5bb413de812d0d6583156920a3dde4719
Yasmany Cubela Medina <ycubela@teravisiontech.com>
2015-09-09 14:49:45 -0430

    working example

--- framework/src/AppBundle/Entity/Client.php
+++ framework/src/AppBundle/Entity/Client.php
@@ -7,4 +7,15 @@ use FOS\OAuthServerBundle\Entity\Client as BaseClient;
 class Client extends BaseClient
 {
     protected $id;
+    protected $name;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
 }
