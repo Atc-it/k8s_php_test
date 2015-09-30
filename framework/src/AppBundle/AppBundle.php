dc9166c6b3c2e325982d4b2ea66ecb073bab417a
Yasmany Cubela Medina <yasmanycm@gmail.com>
2015-09-30 12:22:14 -0430

    removing old auth provider and introduce new storage to FOSOAuthServerBundle

--- framework/src/AppBundle/AppBundle.php
+++ framework/src/AppBundle/AppBundle.php
@@ -8,11 +8,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $extension = $container->getExtension('security');
-        $extension->addSecurityListenerFactory(new RestFactory());
-    }
 }
